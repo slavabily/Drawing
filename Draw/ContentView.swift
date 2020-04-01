2991f7bed2aed7a127707a83b5c0055ac86e3ace
slavabily <slavabily@me.com>
2020-04-01 08:29:56 +0300

    base color cycling functionality is mounted

--- Draw/ContentView.swift
+++ Draw/ContentView.swift
@@ -8,16 +8,6 @@
 
 import SwiftUI
 
-struct Rectangle: Shape {
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        
-        path.addRect(rect)
-        
-        return path
-    }
-}
-
 struct Arrow: Shape {
     
     func path(in rect: CGRect) -> Path {
@@ -40,18 +30,56 @@ struct Arrow: Shape {
     }
 }
 
+struct ColorCyclingRectangle: View {
+    var amount = 0.0
+    var steps = 100
+    
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Rectangle()
+                    .inset(by: CGFloat(value))
+                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [
+                        self.color(for: value, brightness: 1),
+                        self.color(for: value, brightness: 0.5)
+                    ]), startPoint: .top, endPoint: .bottom),  lineWidth: 2)
+            }
+        }
+        .drawingGroup()
+    }
+    
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(self.steps) + self.amount
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
+
 struct ContentView: View {
     @State private var lineWidth: CGFloat = 20
+    @State private var colorcycle = 0.0
      
     var body: some View {
         VStack {
             Arrow()
                 .stroke(Color.blue, style: StrokeStyle(lineWidth: lineWidth, lineCap: .round, lineJoin: .round))
-                .frame(width: 200, height: 200)
+                .frame(width: 100, height: 100)
             
             Spacer(minLength: 50)
             
             Slider(value: $lineWidth, in: 1...40)
+                .padding(.bottom)
+            
+            Spacer(minLength: 50)
+            
+            ColorCyclingRectangle(amount: self.colorcycle)
+                .frame(width: 200, height: 200)
+            
+            Spacer(minLength: 50)
+            
+            Slider(value: $colorcycle)
                 .padding(.bottom)
         }
     }
